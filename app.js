bf9972296667a00cd324144670c6249d6443d97e
Tom Horton <thomasphorton@gmail.com>
2013-12-27 10:51:55 -0500

    socket changes.

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ app.use(express.static(__dirname + '/public'));
 
 var io = require('socket.io').listen(app.listen(port));
 
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
 io.sockets.on('connection', function (socket) {
     socket.emit('message', { message: 'welcome to the chat' });
     socket.on('send', function (data) {
