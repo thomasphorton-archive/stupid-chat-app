03e44e2a4b3fc8980b4c487eea39e64b7afdeec9
Tom Horton <thomasphorton@gmail.com>
2013-12-30 08:53:12 -0500

    First major update. Changed to ejs.

--- app.js
+++ app.js
@@ -1,37 +1,21 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 5000;
- 
 
 app.set('views', __dirname + '/views');
-app.set('view engine', "jade");
-app.engine('jade', require('jade').__express);
+app.set('view engine', 'ejs');
+// app.engine('jade', require('jade').__express);
 app.get("/", function(req, res){
-    res.render("page");
-});
-
-app.get("/foobar", function(req, res) {
-  res.send('foobar');
+    res.render("chat");
 });
 
 app.use(express.static(__dirname + '/public'));
 
 var io = require('socket.io').listen(app.listen(port));
 
-// io.configure(function () { 
-//   io.set("transports", ["xhr-polling"]); 
-//   io.set("polling duration", 10); 
-// });
-
 io.sockets.on('connection', function (socket) {
     console.log('User connected');
     socket.emit('message', { message: 'welcome to the thunderdome' });
-    socket.emit('message', { message: 'the first rule: post post post' });
     socket.on('send', function (data) {
         console.log('Emitting Message')
         io.sockets.emit('message', data);
