326b19b7f3b386af37412f56830efb7b5ef3f85a
Tom Horton <thomasphorton@gmail.com>
2013-12-31 18:07:15 -0500

    commenting out db operations for now

--- app.js
+++ app.js
@@ -21,11 +21,12 @@ app.use(express.static(__dirname + '/public'));
 var pg = require('pg');
 
 pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-  client.query('SELECT * FROM your_table', function(err, result) {
-    done();
-    if(err) return console.error(err);
-    console.log(result.rows);
-  });
+  // client.query('SELECT * FROM your_table', function(err, result) {
+  //   done();
+  //   if(err) return console.error(err);
+  //   console.log(result.rows);
+  // });
+  done();
 });
 
 var io = require('socket.io').listen(app.listen(port));
